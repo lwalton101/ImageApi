a8013368f0abce4833f615f9ba82eebfbe03dec0
lwalton101 <waltonfamily01@outlook.com>
2023-07-26 13:21:33 +0100

    used Nicknotnutils to Refractor AddArrayToList<T>

--- FileManager.cs
+++ FileManager.cs
@@ -1,4 +1,6 @@
-﻿namespace ImageApi;
+﻿using Nicknotnutils.Extension;
+
+namespace ImageApi;
 
 public static class FileManager
 {
@@ -7,14 +9,6 @@ public static class FileManager
     public static void InitFileManager(string path)
     {
         var files = Directory.GetFiles(path + _aminPath, "*", SearchOption.AllDirectories);
-        AddArrayToList(AminPaths, files);
-    }
-
-    private static void AddArrayToList<T>(List<T> list, T[] array)
-    {
-        foreach (var VARIABLE in array)
-        {
-            list.Add(VARIABLE);
-        }
+        AminPaths.AddArray(files);
     }
 }
