934103fa105f3de658a5991ec5f0cea28509f56c
lwalton101 <waltonfamily01@outlook.com>
2023-07-26 14:44:55 +0100

    Get request for Amin Image

--- Controllers/AminImageController.cs
+++ Controllers/AminImageController.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nicknotnutils.Extension;
 
 namespace ImageApi.Controllers;
 
+[ApiController]
+[Route("[controller]")]
 public class AminImageController : ControllerBase
 {
-    
+    [HttpGet()]
+    public ActionResult  Get()
+    {
+        var data = new { path = FileManager.AminPaths.ChooseRandom() };
+        return new JsonResult(data);
+    }
 }
