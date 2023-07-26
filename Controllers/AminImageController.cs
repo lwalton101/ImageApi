e465f47eb7f47a52fc7979393a4a16f68af7c52e
lwalton101 <waltonfamily01@outlook.com>
2023-07-26 19:31:27 +0100

    all of amin api

--- Controllers/AminImageController.cs
+++ Controllers/AminImageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageApi.Model;
+using Microsoft.AspNetCore.Mvc;
 using Nicknotnutils.Extension;
 
 namespace ImageApi.Controllers;
@@ -7,10 +8,64 @@ namespace ImageApi.Controllers;
 [Route("[controller]")]
 public class AminImageController : ControllerBase
 {
-    [HttpGet()]
-    public ActionResult  Get()
+
+    private readonly ILogger<AminImageController> _logger;
+
+    public AminImageController(ILogger<AminImageController> logger)
     {
-        var data = new { path = FileManager.AminPaths.ChooseRandom() };
-        return new JsonResult(data);
+        _logger = logger;
+    }
+
+    [HttpGet()]
+    public ActionResult Get()
+    {
+        var data = new { path = $"https://{Request.Host}{FileManager.AminPaths.ChooseRandom()}" };
+        return new JsonResult(Ok(data));
+    }
+
+    [HttpPut()]
+    public ActionResult Put([FromBody] CreateAminImageModel model)
+    {
+        byte[] bytes;
+        try
+        {
+            bytes = Convert.FromBase64String(model.B64Img);
+        }
+        catch (FormatException)
+        {
+            return new JsonResult(BadRequest("Valid base64 was not sent"));
+        }
+
+        if (!model.ImgName.EndsWith(".png"))
+        {
+            return new JsonResult(BadRequest("File must be a .png"));
+        }
+
+        if (model.B64Img == "")
+        {
+            return new JsonResult(BadRequest("b64Img must not be null"));
+        }
+
+        var imgPath = $"{FileManager.wwwrootPath}/images/amin/{model.ImgName}";
+        if (System.IO.File.Exists(imgPath))
+        {
+            return new JsonResult(Conflict($"File `{model.ImgName}` already exists"));
+        }
+        System.IO.File.WriteAllBytes(imgPath, bytes);
+
+        return new JsonResult(Ok($"File `{model.ImgName}` successfully created`"));
+    }
+
+    [HttpDelete]
+    public ActionResult Delete(string name)
+    {
+        var imgPath = $"{FileManager.wwwrootPath}/images/amin/{name}";
+        if (System.IO.File.Exists(imgPath))
+        {
+            System.IO.File.Delete(imgPath);
+            return new JsonResult(Ok($"File `{name}` has been deleted"));
+        }
+
+        return new JsonResult(NotFound($"File `{name}` cannot be found"));
     }
 }
---
+++ Model/CreateAminImageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ImageApi.Model;
+
+public class CreateAminImageModel
+{
+    public string B64Img { get; set; }
+    public string ImgName { get; set; }
+    
+}
