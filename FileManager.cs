f0e971d13f8c730b6c07cf98cc1c94bda79ffc09
lwalton101 <waltonfamily01@outlook.com>
2023-07-26 14:33:56 +0100

    Fixed FileManager.cs to actually look at the right files

--- FileManager.cs
+++ FileManager.cs
@@ -5,10 +5,13 @@ namespace ImageApi;
 public static class FileManager
 {
     public static List<string> AminPaths = new();
-    private static string _aminPath = "/amin";
-    public static void InitFileManager(string path)
+    private static string _aminPath = "\\images\\amin";
+    public static void InitFileManager(string wwwrootPath)
     {
-        var files = Directory.GetFiles(path + _aminPath, "*", SearchOption.AllDirectories);
-        AminPaths.AddArray(files);
+        var files = Directory.GetFiles(wwwrootPath + _aminPath, "*", SearchOption.AllDirectories);
+        foreach (var file in files)
+        {
+            AminPaths.Add(file.Replace(wwwrootPath, ""));
+        }
     }
 }
