5f6c31c2d9261d0932da77c12a6752866729c6f3
lwalton101 <waltonfamily01@outlook.com>
2023-07-27 16:21:13 +0100

    Added category system to file manager

--- FileManager.cs
+++ FileManager.cs
@@ -4,15 +4,23 @@ namespace ImageApi;
 
 public static class FileManager
 {
-    public static List<string> AminPaths = new();
     private static string _aminPath = "/images/amin";
-    public static string wwwrootPath = "";
+    public static string WwwrootPath = "";
+    public static List<String> ImageCategories = new();
+    public static Dictionary<string, List<string>> ImagesByCategory = new();
     public static void InitFileManager()
     {
-        var files = Directory.GetFiles(wwwrootPath + _aminPath, "*.png", SearchOption.AllDirectories);
-        foreach (var file in files)
+        var dirs = Directory.GetDirectories(WwwrootPath + "/images", "*", SearchOption.TopDirectoryOnly);
+        foreach (var dir in dirs)
         {
-            AminPaths.Add(file.Replace(wwwrootPath, ""));
+            var category = dir.Split("\\").Last();
+            ImageCategories.Add(category);
+            var files = Directory.GetFiles(dir, "*.png", SearchOption.AllDirectories);
+            ImagesByCategory[category] = new List<string>();
+            foreach (var file in files)
+            {
+                ImagesByCategory[category].Add(file.Replace($"{WwwrootPath}/images", $""));
+            }
         }
     }
 }
