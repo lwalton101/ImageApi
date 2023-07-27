eb70066ba6a1fd6dc3d99d55a9178c575c23e721
lwalton101 <waltonfamily01@outlook.com>
2023-07-27 16:21:48 +0100

    Can get image and image categories

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,34 @@
+﻿using ImageApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Nicknotnutils.Extension;
+
+namespace ImageApi.Controllers;
+
+[ApiController]
+[Route("api/image/")]
+public class ImageController : ControllerBase
+{
+    [HttpGet("{category}/")]
+    public ActionResult Get(string category)
+    {
+        if (!FileManager.ImageCategories.Contains(category))
+        {
+            return new JsonResult(NotFound($"Cannot find image under category {category}"));
+        }
+
+        var imageUrl = FileManager.ImagesByCategory[category].ChooseRandom();
+        var response = new GetImageModel
+        {
+            category = category,
+            imageURL = imageUrl
+        };
+
+        return new JsonResult(Ok(response));
+    }
+
+    [HttpGet("categories")]
+    public ActionResult GetCategories()
+    {
+        return new JsonResult(FileManager.ImageCategories);
+    }
+}
---
+++ Model/GetImageModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ImageApi.Model;
+
+public class GetImageModel
+{
+    public string category { get; set; }
+    public string imageURL { get; set; }
+}
